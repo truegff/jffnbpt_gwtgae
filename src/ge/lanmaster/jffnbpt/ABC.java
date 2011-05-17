3ff603e9c5c805327353314b4a9debca50e488c5
Alexander Dolidze <alexander.dolidze@gmail.com>
2011-05-18 02:54:36 +0400

    Added README file. Signed-off-by: Alexander Dolidze <alexander.dolidze@gmail.com>

--- src/ge/lanmaster/jffnbpt/ABC.java
+++ src/ge/lanmaster/jffnbpt/ABC.java
@@ -4,7 +4,8 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("1. Voila, it works!");
         int a = 5;
-        System.out.println("2. Now debug works too!");
+        //toggle breakpoint for the next line and click debug button to see if it works
+        System.out.println("2. Now debug works too!"); 
         a = 7;
         System.out.println("3. ...");
         a = 0;
