9ffa36bf76ad3ca4510f05f83094472af515dbbc
Alexander Dolidze <alexander.dolidze@gmail.com>
2011-05-18 02:04:37 +0400

    Added debugging ability from NB.
    Signed-off-by: Alexander Dolidze <alexander.dolidze@gmail.com>

--- src/ge/lanmaster/JavaProjectTemplate/ABC.java
+++
@@ -1,8 +0,0 @@
-package ge.lanmaster.JavaProjectTemplate;
-
-public class ABC {
-    public static void main(String[] args) {
-        System.out.println("Voila, it works!");
-    }
-}
-
---
+++ src/ge/lanmaster/jffnbpt/ABC.java
@@ -0,0 +1,13 @@
+package ge.lanmaster.jffnbpt;
+
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("1. Voila, it works!");
+        int a = 5;
+        System.out.println("2. Now debug works too!");
+        a = 7;
+        System.out.println("3. ...");
+        a = 0;
+    }
+}
+
