6f0d8898d29eae8b96ed2cdb6cf8f1dfa7ae4e8b
Alexander Dolidze <alexander.dolidze@gmail.com>
2011-06-01 02:39:40 +0400

    gwt version of jffnbpt project
    Signed-off-by: Alexander Dolidze <alexander.dolidze@gmail.com>

---
+++ src/ge/lanmaster/jffnbpt_gwt/client/Start.java
@@ -0,0 +1,13 @@
+package ge.lanmaster.jffnbpt_gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class Start implements EntryPoint {
+
+    public void onModuleLoad() {
+        Window.alert("HOLOLO");
+    }
+      
+}
+
